5537158af1a1fee08325d5206d4de1afcf3bd8c9
vmikhav <mihaw.wolodymyr@gmail.com>
2022-11-28 00:19:16 +0200

    add history

--- scenes/levels/BaseLevel/BaseLevel.gd
+++ scenes/levels/BaseLevel/BaseLevel.gd
@@ -11,11 +11,13 @@ enum Layer {
 var hero_position: Vector2i
 var tilemap: TileMap
 var hero: Node2D
+var allow_input: bool
 var level_items_count: int
 var level_progress: int
 var ghosts = []
 var ghosts_count: int
 var ghosts_progress: int
+var history = []
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,11 +29,15 @@ func init_map(source: Layer = Layer.BAD_ITEMS):
 	tilemap.set_layer_enabled(Layer.GOOD_ITEMS, false)
 	var bad_items = tilemap.get_used_cells(source)
 	if source == Layer.BAD_ITEMS:
+		allow_input = true
+		history = [{position = hero_position}]
 		level_progress = 0
 		level_items_count = bad_items.size()
 		ghosts_progress = 0
 		ghosts_count = ghosts.size()
 		for i in ghosts.size():
+			if "unit" in ghosts[i] and ghosts[i].unit:
+				ghosts[i].unit.queue_free()
 			ghosts[i].unit = hero.duplicate()
 			tilemap.add_child(ghosts[i].unit)
 			ghosts[i].unit.make_ghost()
@@ -54,9 +60,15 @@ func skip_step():
 	print('blocked')
 
 func update_cell(pos: Vector2i, new_value: Vector2i):
-	tilemap.set_cell(Layer.ITEMS, pos, 0, new_value)
+	if new_value.x == -1:
+		tilemap.erase_cell(Layer.ITEMS, pos)
+	else:
+		tilemap.set_cell(Layer.ITEMS, pos, 0, new_value)
 
 func navigate(direction: TileSet.CellNeighbor):
+	if not allow_input:
+		return
+
 	if direction == TileSet.CELL_NEIGHBOR_RIGHT_SIDE:
 		hero.set_orientation('right')
 	if direction == TileSet.CELL_NEIGHBOR_LEFT_SIDE:
@@ -70,6 +82,7 @@ func navigate(direction: TileSet.CellNeighbor):
 		skip_step()
 		return
 	
+	var history_item = {position = neighbor_pos}
 	var neighbor_cell = tilemap.get_cell_atlas_coords(Layer.ITEMS, neighbor_pos) 
 	var bad_neighbor_cell = tilemap.get_cell_atlas_coords(Layer.BAD_ITEMS, neighbor_pos) 
 	var good_neighbor_cell = tilemap.get_cell_atlas_coords(Layer.GOOD_ITEMS, neighbor_pos) 
@@ -78,12 +91,35 @@ func navigate(direction: TileSet.CellNeighbor):
 			skip_step()
 			return
 		update_cell(neighbor_pos, good_neighbor_cell)
+		history_item.bad_item = bad_neighbor_cell
+		history_item.good_item = good_neighbor_cell
 		level_progress += 1
 	for i in ghosts.size():
 		if neighbor_pos == ghosts[i].position:
 			ghosts_progress += 1
-			ghosts[i].unit.queue_free()
+			ghosts[i].unit.visible = false
+			history_item.ghost = ghosts[i].unit
 			ghosts.remove_at(i)
 			break
 	move_hero_to_position(neighbor_pos)
-		
+	history.push_back(history_item)
+	if ghosts_progress == ghosts_count:
+		replay()
+	
+
+func replay():
+	history.reverse()
+	for history_item in history:
+		if history_item.position.x > hero_position.x:
+			hero.set_orientation('right')
+		if history_item.position.x < hero_position.x:
+			hero.set_orientation('left')
+		move_hero_to_position(history_item.position)
+		if "bad_item" in history_item:
+			update_cell(history_item.position, history_item.bad_item)
+			level_progress -= 1
+		if "ghost" in history_item:
+			history_item.ghost.visible = true
+			ghosts.push_back({position = history_item.position, unit = history_item.ghost})
+			ghosts_progress -= 1
+	history = [{position = hero_position}]

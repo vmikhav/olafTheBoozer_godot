a45a8a96925b2d98ea297030395f9053c3a81794
vmikhav <mihaw.wolodymyr@gmail.com>
2022-11-27 16:06:06 +0200

    navigation ongoing

--- scenes/levels/BaseLevel/BaseLevel.gd
+++ scenes/levels/BaseLevel/BaseLevel.gd
@@ -1,14 +1,39 @@
 class_name BaseLevel
 extends Node2D
 
+const TILE_SIZE = 16
+const TILE_OFFSET = Vector2i(8, 10)
+
 enum LAYERS {
 	GROUND, FLOOR, WALLS, ITEMS, TREES, BAD_ITEMS, GOOD_ITEMS
 }
 
+var hero_position: Vector2i
 var tilemap: TileMap
-
-
+var hero: Node2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func move_hero_to_position(position: Vector2i):
+	if hero_position.x > position.x:
+		hero.set_orientation('right')
+	
+	if hero_position.x < position.x:
+		hero.set_orientation('left')
+	
+	hero_position = position
+	hero.position = position * TILE_SIZE + TILE_OFFSET
+
+
+func skip_step():
+	pass
+
+func navigate(direction: TileSet.CellNeighbor):
+	if direction == TileSet.CELL_NEIGHBOR_RIGHT_SIDE:
+		hero.set_orientation('right')
+	if direction == TileSet.CELL_NEIGHBOR_LEFT_SIDE:
+		hero.set_orientation('left')
+	
+	
--- scenes/levels/Tutorial0/Tutorial0.gd
+++ scenes/levels/Tutorial0/Tutorial0.gd
@@ -4,7 +4,10 @@ extends BaseLevel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tilemap = $TileMap
+	tilemap = $TileMap as TileMap
+	hero = $TileMap/Demolitonist
+	hero_position = Vector2i(3, 4)
+	move_hero_to_position(hero_position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

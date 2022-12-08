48864ddc984293d2ff65c09714fcebdfafea4e4a
vmikhav <mihaw.wolodymyr@gmail.com>
2022-12-08 14:52:46 +0200

    fix await on scene change

--- scenes/ui/SceneTransition/SceneTransition.gd
+++ scenes/ui/SceneTransition/SceneTransition.gd
@@ -5,15 +5,14 @@ func fade_in():
 	modulate.a8 = 255
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate", Color8(255, 255, 255, 0), 0.5)
-	return tween.finished
+	await tween.finished
 
 func fade_out():
 	modulate.a8 = 0
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate", Color8(255, 255, 255, 255), 0.5)
-	return tween.finished
+	await tween.finished
 
 func change_scene(scene: String, params = null):
-	var tween = fade_out()
-	await tween.finished
+	await fade_out()
 	SceneSwitcher.change_scene_to_file(scene, params)
